1b1ccf84bbeab5d97a1e05259b982e7837365f0a
MD Asif <109533829+asif00b@users.noreply.github.com>
2022-11-29 04:11:26 +0600

    nav and hero section completed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var typed3 = new Typed(".type", {
+  strings: [
+    "Let's make ourselves <br> <i>efficient</i>",
+    "Let's make ourselves <br> <strong>skilled</strong>",
+    "Let's make ourselves <br> expert",
+  ],
+  typeSpeed: 60,
+  backSpeed: 60,
+  smartBackspace: true, // this is a default
+  loop: true,
+});
