0e7a4ba341dd4152460bd91cba46e80dddf18109
MD Asif <109533829+asif00b@users.noreply.github.com>
2022-12-04 07:41:21 +0600

    completed main webpage without responsive

--- js/script.js
+++ js/script.js
@@ -4,6 +4,50 @@ setTimeout(function () {
   $("#pre").fadeToggle();
 }, 2500);
 
+// body bg
+// Some random colors
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 80;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` },
+    ],
+    {
+      duration: (Math.random() + 1) * 3000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    }
+  );
+});
+
+// scrool nav
 window.addEventListener("scroll", function () {
   let navBar = this.document.querySelector("nav");
   if (this.window.pageYOffset >= 600) {
@@ -13,6 +57,7 @@ window.addEventListener("scroll", function () {
   }
 });
 
+// typed js
 var typed3 = new Typed(".type", {
   strings: [
     "Let's make ourselves <br> <i>efficient</i>",
@@ -75,6 +120,135 @@ for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener("click", animateButton, false);
 }
 // slider
+// venobox
 
+new VenoBox({
+  selector: ".my-video-links",
+});
+
+// counter
+let totalNumber = document.querySelectorAll(".counter-text");
+let getNumber = Array.from(totalNumber);
+
+getNumber.map((viewNumber) => {
+  console.log(viewNumber.dataset.number);
+  let startCount = 0;
+  let counterUP = () => {
+    startCount++;
+    viewNumber.innerHTML = `${startCount}`;
+    if (startCount == viewNumber.dataset.number) {
+      clearInterval(countStop);
+    }
+  };
+  let countStop = setInterval(() => {
+    counterUP();
+  }, 13);
+});
+
+// counter
 // aos
 AOS.init();
+
+// testimoni slider
+
+jQuery(document).ready(function ($) {
+  var feedbackSlider = $(".feedback-slider");
+  feedbackSlider.owlCarousel({
+    items: 1,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    navText: [
+      "<i class='fa fa-long-arrow-left'></i>",
+      "<i class='fa fa-long-arrow-right'></i>",
+    ],
+    responsive: {
+      // breakpoint from 767 up
+      767: {
+        nav: true,
+        dots: false,
+      },
+    },
+  });
+
+  feedbackSlider.on("translate.owl.carousel", function () {
+    $(".feedback-slider-item h3")
+      .removeClass("animated fadeIn")
+      .css("opacity", "0");
+    $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
+      .removeClass("animated zoomIn")
+      .css("opacity", "0");
+  });
+
+  feedbackSlider.on("translated.owl.carousel", function () {
+    $(".feedback-slider-item h3")
+      .addClass("animated fadeIn")
+      .css("opacity", "1");
+    $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
+      .addClass("animated zoomIn")
+      .css("opacity", "1");
+  });
+  feedbackSlider.on("changed.owl.carousel", function (property) {
+    var current = property.item.index;
+    var prevThumb = $(property.target)
+      .find(".owl-item")
+      .eq(current)
+      .prev()
+      .find("img")
+      .attr("src");
+    var nextThumb = $(property.target)
+      .find(".owl-item")
+      .eq(current)
+      .next()
+      .find("img")
+      .attr("src");
+    var prevRating = $(property.target)
+      .find(".owl-item")
+      .eq(current)
+      .prev()
+      .find("span")
+      .attr("data-rating");
+    var nextRating = $(property.target)
+      .find(".owl-item")
+      .eq(current)
+      .next()
+      .find("span")
+      .attr("data-rating");
+    $(".thumb-prev").find("img").attr("src", prevThumb);
+    $(".thumb-next").find("img").attr("src", nextThumb);
+    $(".thumb-prev")
+      .find("span")
+      .next()
+      .html(prevRating + '<i class="fa fa-star"></i>');
+    $(".thumb-next")
+      .find("span")
+      .next()
+      .html(nextRating + '<i class="fa fa-star"></i>');
+  });
+  $(".thumb-next").on("click", function () {
+    feedbackSlider.trigger("next.owl.carousel", [300]);
+    return false;
+  });
+  $(".thumb-prev").on("click", function () {
+    feedbackSlider.trigger("prev.owl.carousel", [300]);
+    return false;
+  });
+}); //end ready
+
+// swiper contact
+var swiper = new Swiper(".blog-slider", {
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: ".blog-slider__pagination",
+    clickable: true,
+  },
+});
